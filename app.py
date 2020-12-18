bae3d3f67c09a5d304618ece4ecedbe1fa88abb9
Justharshh <67090038+Justharshh@users.noreply.github.com>
2020-12-19 03:07:13 +0530

    19-12

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template
 import joblib
-from prettytable import PrettyTable
-
-
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 
 model = joblib.load("Population_data_cluster.pkl")
 
 df = pd.DataFrame()
+
+db = yaml.load(open("db.yaml"))
+app.config["MYSQL_HOST"] = db["mysql_host"]
+app.config["MYSQL_USER"] = db["mysql_user"]
+app.config["MYSQL_PASSWORD"] = db["mysql_password"]
+app.config["MYSQL_DB"] = db["mysql_db"]
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -29,18 +36,7 @@ def screenSnaps():
 @app.route('/about.html')
 def about():
     return render_template('about.html')
-# Route for handling the login page logic
-@app.route('/login.html', methods=['GET', 'POST'])
-def login():
 
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'popcluster' or request.form['password'] != 'popcluster':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return render_template('Tablee.html',)
-    return render_template('login.html', error=error)
-    
 @app.route('/predict',methods=['POST'])
 def predict():
     global df
@@ -51,11 +47,17 @@ def predict():
     foreigners = request.form['foreigners']
     indian_male = request.form['indians_male']
     indian_female = request.form['indian_females']
-    foreigns_male = request.form['foreigners_male']
+    foreigners_male = request.form['foreigners_male']
     foreigners_female = request.form['foreigners_females']
     other  =  request.form['Others']       
-    output = model.predict([[indians,foreigners,indian_male,indian_female,foreigns_male,foreigners_female,other]])[0]
+    output = model.predict([[indians,foreigners,indian_male,indian_female,foreigners_male,foreigners_female,other]])[0]
+    output+=1
     alert="Submission Successfull Please Scroll Down for results"
+
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users(Indians, Foreigners, Indian_Males, Indian_Females, Foreigner_Males, Foreigner_Females, Others, Cluster) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)",(indians,foreigners,indian_male,indian_female,foreigners_male,foreigners_female,other,output))
+    mysql.connection.commit()
+    cur.close()
     # # #validate input hours
     # if (indians=="" or foreigners=="" or foreigners=="" or indian_male=="" or indian_female=="" or foreigns_male=="" or foreigners_female=="" or other=="" ):
     #     return render_template('index.html', prediction_text='Missing Values : All fields are mandatory to fill')
@@ -66,40 +68,31 @@ def predict():
     #input and predicted value store in df then save in csv file
     df= pd.concat([df,pd.DataFrame({'Indians':indians,'Foreigners':foreigners,
                                     'Indians male':indian_male,'Indian Females':indian_female,
-                                    'Foreigners Male':foreigns_male,'Foreigners Females':foreigners_female,
+                                    'Foreigners Male':foreigners_male,'Foreigners Females':foreigners_female,
                                     'Others':other,
-                                    'Predicted Output':[output]})],ignore_index=True)
+                                    'Cluster':[output]})],ignore_index=True)
     print(df)   
     df.to_csv('smp_data_from_app.csv')
 
-   
-        # open csv file 
-    a = open("smp_data_from_app.csv", 'r') 
     
-    # read the csv file 
-    a = a.readlines() 
+#################################################################################################   
+
+    return render_template('index.html', prediction_text=f"Cluster : {output} out of 3",stat= f" FOR Indians : {indians}, Foreigners : {foreigners}, Indians male : {indian_male},Indian Females : {indian_female}, Foreigners Male : {foreigners_male}, Foreigners Females : {foreigners_female},Others : {other} ",alert=alert)
+
+# Route for handling the login page logic
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    userDetails = cur.fetchall()
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'popcluster' or request.form['password'] != 'popcluster':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template('Tablee.html',userDetails=userDetails)
+    return render_template('login.html', error=error)
     
-    # Seperating the Headers 
-    l1 = a[0] 
-    l1 = l1.split(',') 
-    
-    # headers for table 
-    t = PrettyTable([l1[0],l1[1],l1[2],l1[3],l1[4],l1[5],l1[6],l1[7],l1[8]]) 
-    
-    # Adding the data 
-    for i in range(1, len(a)) : 
-        t.add_row(a[i].split(',')) 
-    code = t.get_html_string(attributes={"name":"my_table", "class":"red_table", "style":"border:2px solid black"}) 
-    html_file = open('templates/Tablee.html', 'w') 
-    html_file = html_file.write(code) 
-
-
-
-
-    
-
-    return render_template('index.html', prediction_text=f"Cluster : {output} out of 3"+ f" FOR Indians : {indians}, Foreigners : {foreigners}, Indians male : {indian_male},Indian Females : {indian_female}, Foreigners Male : {foreigns_male}, Foreigners Females : {foreigners_female},Others : {other} ",alert=alert)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
